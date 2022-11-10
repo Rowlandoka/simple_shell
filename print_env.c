1814290792b3b3c6aed4afe7f45a10901bce96bc
Rowland Oka <rowlandoka@gmail.com>
2022-11-10 16:04:58 +0100

    print environment variables

---
+++ print_env.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * print_env - print environment strings to stdout
+ * 
+ * Return: 0
+ */
+
+void print_env(void)
+{
+    int y = 0;
+
+    char **env = environ;
+
+    while (env[y])
+    {
+        write(STDOUT_FILENO, (const void *)env[y], _strlen(env[y]));
+        write(STDOUT_FILENO, "\n", 1);
+        y++;
+    }
+}
